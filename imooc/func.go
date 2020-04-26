2bce3b8bb2f89273986c363bef18a4d845f1b93d
刘建超 <2167162990@qq.com>
2020-04-26 12:46:20 +0800

    函数

--- imooc/func.go
+++ imooc/func.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
+// 函数
+func calculator(a, b int, op string) int {
+	switch op {
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "*":
+		return a * b
+	case "/":
+		return a / b
+	default:
+		panic("报错：" + op)
+	}
+}
+
 func main() {
-	$END$
+	fmt.Println(calculator(1, 2, "+"), calculator(1, 2, "/"))
 }

983f3087859627d823315b4779a2b064d7fc6916
超大大 <2167162990@qq.com>
2020-04-29 16:26:23 +0800

    GOPATH的学习

--- imooc/GOPATH.go
+++ imooc/GOPATH.go
@@ -1,15 +1,20 @@
 package main
 
+//import "fmt"  // 系统的库
+//import "tree" // 自己的库
+
 func main() {
-
-
 
 	// 刚安装完go 的时候 GOPATH 是不会被设置的
 	// 如果不设置 就会默认在本地的 用户目录下 go
 	// 建议 自己设置一下 GOPATH
 
-
-
 	// 官方推荐所有的项目 和第三方库都在同一个 GOPATH 下
 	// 也可以将每个项目放在不同的 GOPATH 下
+
+	// go fmt 格式化我们的文件
+	// go imports  整理import的工具
+
+	// go get golang.org/x/tools/cmd/goimports 获取第三方库
+
 }
